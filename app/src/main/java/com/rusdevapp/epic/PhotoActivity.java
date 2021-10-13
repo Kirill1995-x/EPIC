86ed452266dcf2c0cdd41a1bb92b33e94e29531b
Kirill1995-x <70485074+Kirill1995-x@users.noreply.github.com>
2021-10-13 13:29:51 +0300

    1. Changed algorithm of showing EPIC and title in PhotoActivity

--- app/src/main/java/com/rusdevapp/epic/PhotoActivity.java
+++ app/src/main/java/com/rusdevapp/epic/PhotoActivity.java
@@ -1,12 +1,18 @@
 package com.rusdevapp.epic;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.app.WallpaperManager;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.rusdevapp.epic.databinding.ActivityPhotoBinding;
 
 import java.io.IOException;
@@ -20,16 +26,37 @@ public class PhotoActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         binding=ActivityPhotoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-            Bundle bundle = getIntent().getExtras();
-            String date = bundle.getString("date");
-            String name = bundle.getString("image") + ".jpg?api_key=";
-            String url = App.PHOTO_URL + date + "/jpg/" + name + App.API_KEY;
-            Glide.with(this)
-                    .load(url)
-                    .placeholder(R.drawable.progress_animation)
-                    .error(R.drawable.ic_error)
-                    .into(binding.imgPhoto);
-            binding.tvPhoto.setText(bundle.getString("image"));
+        Bundle bundle = getIntent().getExtras();
+        String date = bundle.getString("date");
+        String name = bundle.getString("image") + ".jpg?api_key=";
+        String url = App.PHOTO_URL + date + "/jpg/" + name + App.API_KEY;
+        Glide.with(this)
+                .load(url)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e,
+                                                Object model, Target<Drawable> target,
+                                                boolean isFirstResource)
+                    {
+                        binding.pbPhoto.setVisibility(View.GONE);
+                        return false;
+                    }
+                    @Override
+                    public boolean onResourceReady(Drawable resource,
+                                                   Object model,
+                                                   Target<Drawable> target,
+                                                   DataSource dataSource,
+                                                   boolean isFirstResource)
+                    {
+                        binding.pbPhoto.setVisibility(View.GONE);
+                        binding.tvPhoto.setVisibility(View.VISIBLE);
+                        binding.btnSetPhoto.setVisibility(View.VISIBLE);
+                        return false;
+                    }
+                })
+                .error(R.drawable.nophoto)
+                .into(binding.imgPhoto);
+        binding.tvPhoto.setText(bundle.getString("image"));
         binding.btnSetPhoto.setOnClickListener(this);
     }
 
